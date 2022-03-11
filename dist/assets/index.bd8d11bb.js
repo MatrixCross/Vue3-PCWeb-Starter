ec580ad4c1037275931e62bedecc5f7a5143e957
linhy29 <linhy29@midea.com>
2022-03-11 14:33:47 +0800

    build

---
+++ dist/assets/index.bd8d11bb.js
@@ -0,0 +1,1 @@
+import{b as n,x as s,y as u,Z as p,k as c}from"./vendor.8984c2d2.js";var m=n({name:"RedirectComponent",setup(){const o=s(),r=u();return p(()=>{const{params:t,query:a}=o,{path:e}=t;r.replace({path:"/"+(Array.isArray(e)?e.join("/"):e),query:a})}),()=>c("div",null,null)}});export{m as default};
